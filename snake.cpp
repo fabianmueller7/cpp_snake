44f349577ef9ceff8514ec634a633c743d75c801
muellefa <fabian.mueller@schindler.com>
2022-04-25 10:14:11 +0200

    displayfield

--- snake.cpp
+++ snake.cpp
@@ -4,23 +4,77 @@
 #include <vector>
 using namespace std;
 
-void display() 
-{
-
+void clear() {
+    // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
+    std::cout << "\x1B[2J\x1B[H";
 }
 
-void gameloop(int **field, int snakelength, int fieldheight, int fieldwidth) 
+void gotoxy(int x, int y) {
+    COORD c;
+    c.X=x;
+    c.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
+}
+
+void display(int **field, int height, int width) 
 {
-    while(true) {
-        cout << "Number: ";
-        cout << field[10][10];
+        for (int h = 0; h < height; h++)
+    {
+        cout << endl;
+
+        for (int w = 0; w < width; w++)
+        {
+            gotoxy(w + 1, h + 1);
+            if(field[h][w] > 0)
+            {
+                cout << "S";
+            }
+            else if(field[h][w] < 0)
+            {
+                cout << "F";
+            }
+            else {
+                cout << " ";
+            }
+        }
+    }
+}
+
+void updatefield(int **field, int snakelength, int height, int width, COORD cposition)
+{
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            if(field[h][w] > 0)
+            {
+                field[h][w] -= 1;
+            }
+        }
+    }
+
+    field[cposition.Y][cposition.X] = snakelength;
+}
+
+void gameloop(int **field, int startinglength, int fieldheight, int fieldwidth, COORD startingposition) 
+{
+    int length = startinglength;
+    COORD cposition = startingposition;
+
+    clear();
+
+    while(true) 
+    {
+        Sleep(500);
+        updatefield(field, length, fieldheight, fieldwidth, cposition);
+        display(field, fieldheight, fieldwidth);
     }
 }
 
 int** setupfield(int height, int width) 
 {
     int** field = 0;
-
+    field = new int*[height]; 
     for (int h = 0; h < height; h++)
     {
         field[h] = new int[width];
@@ -32,7 +86,7 @@ int** setupfield(int height, int width)
     }
 
     //add a fruit
-    field[20][20] = -1; 
+    field[5][5] = -1; 
 
     return field;
 } 
@@ -40,10 +94,12 @@ int** setupfield(int height, int width)
 void startgame() 
 {
     int snakelength = 4;
-    int fieldheight = 100;
-    int fieldwidth = 100;
-    gameloop(setupfield(fieldheight,fieldwidth), snakelength, fieldheight, fieldwidth);
-
+    int fieldheight = 10;
+    int fieldwidth = 10;
+    COORD startingposition;
+    startingposition.X = 8;
+    startingposition.Y = 5;
+    gameloop(setupfield(fieldheight,fieldwidth), snakelength, fieldheight, fieldwidth, startingposition);
 }
 
 int main() 
